fcd08aee85f3cbfa3ea3951c0ca2e331261a40ed
Brian D Nelson <brian.nelson@purplewave.com>
2023-11-21 14:35:15 -0600

    Add routes to handle test cases

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,58 @@
 package main
 
-import(
-    "fmt"
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/httplog/v2"
+	"log/slog"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 func main() {
-    fmt.Println("Hello")
+	fmt.Println("Hello")
+
+	logger := httplog.NewLogger("dummy-service", httplog.Options{
+		LogLevel:        slog.LevelDebug,
+		Concise:         false,
+		RequestHeaders:  true,
+		ResponseHeaders: true,
+	})
+
+	r := chi.NewRouter()
+	r.Use(httplog.RequestLogger(logger))
+	r.Get("/success", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Success"))
+	})
+	r.Get("/notfound", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, http.StatusText(404), 404)
+	})
+	r.Get("/error", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, http.StatusText(400), 400)
+	})
+	r.Get("/error/{code}", func(w http.ResponseWriter, r *http.Request) {
+		codeString := chi.URLParam(r, "code")
+		code, err := strconv.Atoi(codeString)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+		} else {
+			http.Error(w, http.StatusText(code), code)
+		}
+	})
+	r.Get("/timeout", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(30 * time.Second)
+		w.Write([]byte("done after 30 seconds"))
+	})
+	r.Get("/timeout/{duration}", func(w http.ResponseWriter, r *http.Request) {
+		durationString := chi.URLParam(r, "duration")
+		duration, err := strconv.Atoi(durationString)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+		} else {
+			time.Sleep(time.Duration(duration) * time.Second)
+			w.Write([]byte(fmt.Sprintf("done after %d seconds", duration)))
+		}
+	})
+	http.ListenAndServe(":9292", r)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module dummy-service
 
 go 1.21.4
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/go-chi/httplog/v2 v2.0.7 // indirect
+)
